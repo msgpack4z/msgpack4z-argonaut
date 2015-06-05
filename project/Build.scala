e297e88ac5d43bae508d8512282b08b6a7786774
xuwei-k <6b656e6a69@gmail.com>
2015-06-05 09:22:41 +0900

    scalaprops

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,5 @@
 import sbt._, Keys._
+import scalaprops.ScalapropsPlugin.autoImport._
 
 object build extends Build {
 
@@ -6,13 +7,14 @@ object build extends Build {
   val modules = msgpack4zArgonautName :: Nil
 
   lazy val msgpack4z = Project("msgpack4z-argonaut", file(".")).settings(
-    Common.settings: _*
+    Common.settings ++ scalapropsSettings
   ).settings(
     name := msgpack4zArgonautName,
+    scalapropsVersion := "0.1.7-SNAPSHOT",
+    resolvers += Opts.resolver.sonatypeSnapshots,
     libraryDependencies ++= (
       ("io.argonaut" %% "argonaut" % "6.1") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.2") ::
-      ("org.scalacheck" %% "scalacheck" % "1.12.2" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.3" % "test").exclude("org.msgpack", "msgpack-core") ::
       ("org.msgpack" % "msgpack-core" % "0.7.0-p9" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
--- src/test/scala/msgpack4z/Spec.scala
+++ src/test/scala/msgpack4z/Spec.scala
@@ -3,78 +3,82 @@ package msgpack6z
 import argonaut._
 import argonaut.Json.JsonArray
 import msgpack4z._
-import org.scalacheck.{Gen, Arbitrary, Prop, Properties}
+import scalaprops._
 import scalaz.{-\/, Equal, \/-}
 import scalaz.std.list._
 
-abstract class SpecBase(name: String) extends Properties(name){
+sealed abstract class SpecBase extends Scalaprops {
 
-  private val bigDecimalGen: Gen[BigDecimal] =
-    gen[Double].map(BigDecimal(_))
+  private[this] implicit val scalaDoubleGen: Gen[Double] =
+    Gen[Long].map { n =>
+      java.lang.Double.longBitsToDouble(n) match {
+        case x if x.isNaN => n
+        case x => x
+      }
+    }
 
-  private val jsonNumberGen: Gen[JsonNumber] =
+  private[this] implicit val bigDecimalGen: Gen[BigDecimal] =
+    Gen[Double].map(BigDecimal(_))
+
+  private[this] implicit val stringGen = Gen.alphaNumString
+
+  private[this] val jsonNumberGen: Gen[JsonNumber] =
     Gen.oneOf(
       bigDecimalGen.map(JsonBigDecimal),
-      gen[Long].map(JsonLong),
-      gen[Double].map(JsonDouble),
+      Gen[Long].map(JsonLong),
+      Gen[Double].map(JsonDouble),
       bigDecimalGen.map(a => JsonNumber.fromString(a.toString).get)
     )
 
-  private val jsValuePrimitivesArb: Arbitrary[Json] =
-    Arbitrary(Gen.oneOf(
-      Gen.const(Json.jNull),
-      Gen.const(Json.jTrue),
-      Gen.const(Json.jFalse),
+  private[this] val jsValuePrimitivesArb: Gen[Json] =
+    Gen.oneOf(
+      Gen.value(Json.jNull),
+      Gen.value(Json.jTrue),
+      Gen.value(Json.jFalse),
       jsonNumberGen.map(Json.jNumber(_)),
-      gen[String].map(Json.jString)
-    ))
+      Gen[String].map(Json.jString)
+    )
 
-  private val jsObjectArb1: Arbitrary[JsonObject] =
-    Arbitrary(Gen.choose(0, 6).flatMap(n =>
+  private[this] val jsObjectArb1: Gen[JsonObject] =
+    Gen.choose(0, 6).flatMap(n =>
+      Gen.sequenceNList(
+        n,
+        Gen.tuple2(
+          Gen[String], jsValuePrimitivesArb
+        )
+      ).map(JsonObject.from(_))
+    )
+
+  private[this] val jsArrayArb1: Gen[JsonArray] =
+    Gen.choose(0, 6).flatMap(n =>
+      Gen.listOfN(n, jsValuePrimitivesArb)
+    )
+
+  implicit val jsValueArb: Gen[Json] =
+    Gen.oneOf(
+      jsValuePrimitivesArb,
+      jsObjectArb1.map(Json.jObject),
+      jsArrayArb1.map(Json.jArray)
+    )
+
+  implicit val jsObjectArb: Gen[JsonObject] =
+    Gen.choose(0, 6).flatMap(n =>
       Gen.listOfN(
         n,
-        Arbitrary.arbTuple2(
-          arb[String], jsValuePrimitivesArb
-        ).arbitrary
+        Gen.tuple2(Gen[String], jsValueArb)
       ).map(JsonObject.from(_))
-    ))
+    )
 
-  private val jsArrayArb1: Arbitrary[JsonArray] =
-    Arbitrary(Gen.choose(0, 6).flatMap(n =>
-      Gen.listOfN(n, jsValuePrimitivesArb.arbitrary)
-    ))
-
-  implicit val jsValueArb: Arbitrary[Json] =
-    Arbitrary(Gen.oneOf(
-      jsValuePrimitivesArb.arbitrary,
-      jsObjectArb1.arbitrary.map(Json.jObject),
-      jsArrayArb1.arbitrary.map(Json.jArray)
-    ))
-
-  implicit val jsObjectArb: Arbitrary[JsonObject] =
-    Arbitrary(Gen.choose(0, 6).flatMap(n =>
-      Gen.listOfN(
-        n,
-        Arbitrary.arbTuple2(arb[String], jsValueArb).arbitrary
-      ).map(JsonObject.from(_))
-    ))
-
-  implicit val jsArrayArb: Arbitrary[JsonArray] =
-    Arbitrary(Gen.choose(0, 6).flatMap(n =>
-      Gen.listOfN(n, jsValueArb.arbitrary)
-    ))
-
-  final def gen[A: Arbitrary]: Gen[A] =
-    implicitly[Arbitrary[A]].arbitrary
-
-  final def arb[A: Arbitrary]: Arbitrary[A] =
-    implicitly[Arbitrary[A]]
+  implicit val jsArrayArb: Gen[JsonArray] =
+    Gen.choose(0, 6).flatMap(n =>
+      Gen.listOfN(n, jsValueArb)
+    )
 
   protected[this] def packer(): MsgPacker
   protected[this] def unpacker(bytes: Array[Byte]): MsgUnpacker
 
-  private def checkRoundTripBytes[A](implicit A: MsgpackCodec[A], G: Arbitrary[A], E: Equal[A]) =
-    Prop.forAll { a: A =>
+  private[this] def checkRoundTripBytes[A](implicit A: MsgpackCodec[A], G: Gen[A], E: Equal[A]) =
+    Property.forAll { a: A =>
       A.roundtripz(a, packer(), unpacker _) match {
         case None =>
           true
@@ -87,21 +91,21 @@ abstract class SpecBase(name: String) extends Properties(name){
       }
     }
 
-  property("Json") = {
+  val testJson = {
     implicit val instance = ArgonautMsgpack.jsonCodec(
       ArgonautUnpackOptions.default
     )
     checkRoundTripBytes[Json]
   }
 
-  property("JsonObject") = {
+  val testJsonObject = {
     implicit val instance = ArgonautMsgpack.jsonObjectCodec(
       ArgonautUnpackOptions.default
     )
     checkRoundTripBytes[JsonObject]
   }
 
-  property("JsonArray") = {
+  val testJsonArray = {
     implicit val instance = ArgonautMsgpack.jsonArrayCodec(
       ArgonautUnpackOptions.default
     )
@@ -109,17 +113,17 @@ abstract class SpecBase(name: String) extends Properties(name){
   }
 }
 
-object Java06Spec extends SpecBase("java06"){
+object Java06Spec extends SpecBase {
   override protected[this] def packer() = Msgpack06.defaultPacker()
   override protected[this] def unpacker(bytes: Array[Byte]) = Msgpack06.defaultUnpacker(bytes)
 }
 
-object Java07Spec extends SpecBase("java07"){
+object Java07Spec extends SpecBase {
   override protected[this] def packer() = new Msgpack07Packer()
   override protected[this] def unpacker(bytes: Array[Byte]) = Msgpack07Unpacker.defaultUnpacker(bytes)
 }
 
-object NativeSpec extends SpecBase("native"){
+object NativeSpec extends SpecBase {
   override protected[this] def packer() = MsgOutBuffer.create()
   override protected[this] def unpacker(bytes: Array[Byte]) = MsgInBuffer(bytes)
 }
