60d52024db24228c7d0ef063f6bc699bd4181a51
xuwei-k <6b656e6a69@gmail.com>
2015-07-04 01:39:58 +0900

    scalaprops 0.1.10

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object build extends Build {
     Common.settings ++ scalapropsSettings
   ).settings(
     name := msgpack4zArgonautName,
-    scalapropsVersion := "0.1.9",
+    scalapropsVersion := "0.1.10",
     libraryDependencies ++= (
       ("io.argonaut" %% "argonaut" % "6.1") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.3") ::
