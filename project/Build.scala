b6e25b70f39ffa3e624411c83fd5f70bffbf8d42
xuwei-k <6b656e6a69@gmail.com>
2015-07-29 11:11:05 +0900

    "msgpack4z-core" % "0.1.4"

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object build extends Build {
     scalapropsVersion := "0.1.11",
     libraryDependencies ++= (
       ("io.argonaut" %% "argonaut" % "6.1") ::
-      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.3") ::
+      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.4") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.4" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
