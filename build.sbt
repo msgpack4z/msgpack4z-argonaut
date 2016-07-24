485ffa19281decfac861389bea9ba083a5511a59
xuwei-k <6b656e6a69@gmail.com>
2016-07-25 01:29:53 +0900

    sbt 0.13.12

---
+++ build.sbt
@@ -0,0 +1,20 @@
+import build._
+
+Common.settings
+
+scalapropsSettings
+
+name := msgpack4zArgonautName
+
+scalapropsVersion := "0.3.2"
+
+libraryDependencies ++= (
+  ("io.argonaut" %% "argonaut" % "6.1a") ::
+  ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.3") ::
+  ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.3" % "test") ::
+  ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
+  ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
+  Nil
+)
+
+Sxr.subProjectSxr(Compile, "classes.sxr")
--- project/Build.scala
+++ project/Build.scala
@@ -1,26 +1,6 @@
-import sbt._, Keys._
-import scalaprops.ScalapropsPlugin.autoImport._
+object build {
 
-object build extends Build {
-
-  private val msgpack4zArgonautName = "msgpack4z-argonaut"
+  val msgpack4zArgonautName = "msgpack4z-argonaut"
   val modules = msgpack4zArgonautName :: Nil
-
-  lazy val msgpack4z = Project("msgpack4z-argonaut", file(".")).settings(
-    Common.settings ++ scalapropsSettings
-  ).settings(
-    name := msgpack4zArgonautName,
-    scalapropsVersion := "0.3.2",
-    libraryDependencies ++= (
-      ("io.argonaut" %% "argonaut" % "6.1a") ::
-      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.3") ::
-      ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.3" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
-      ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
-      Nil
-    )
-  ).settings(
-    Sxr.subProjectSxr(Compile, "classes.sxr"): _*
-  )
 
 }
