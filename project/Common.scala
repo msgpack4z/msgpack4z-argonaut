5217bbad143a17ea4b6a87e478fa13a2ac95d813
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2025-11-17 11:11:04 +0000

    Update scala-library to 2.13.18

--- project/Common.scala
+++ project/Common.scala
@@ -24,7 +24,7 @@ object Common {
   )
 
   val Scala212 = "2.12.20"
-  private[this] val Scala213 = "2.13.17"
+  private[this] val Scala213 = "2.13.18"
   private[this] val Scala3 = "3.3.7"
 
   val settings = Seq(
